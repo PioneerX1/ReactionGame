72a9994a7c7a45f890163b24b969187484ef3bcd
Mick Sexton <lacrookedbeat@hotmail.com>
2018-03-27 19:55:45 -0700

    invalidate countdown timer and start a score accruing timer

--- ReactionGame/ViewController.m
+++ ReactionGame/ViewController.m
@@ -37,8 +37,15 @@
     self.getReadyLabel.text = [NSString stringWithFormat:@"%i", countDown];
     if (countDown == 0) {
         [timerCountDown invalidate];
+        countScore = 0;
+        self.timerLabel.text = [NSString stringWithFormat:@"%i", countScore];
+        timerScore = [NSTimer scheduledTimerWithTimeInterval:0.001 target:self selector:@selector(countingScore) userInfo:nil repeats:YES];
     }
 }
 
+-(void)countingScore {
+    countScore++;
+    self.timerLabel.text = [NSString stringWithFormat:@"%i", countScore];
+}
 
 @end
